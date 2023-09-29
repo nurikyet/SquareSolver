12655c7ee3396714529678046156c21e4ccb55d0
Alexander Paniman <alexpaniman@gmail.com>
2023-09-30 02:13:49 +0300

    Review code

--- Hash.h
+++ Hash.h
@@ -2,14 +2,28 @@
 #define HASH_H_INCLUDED
 
 #include "Types.h"
+// TODO:                                       This function be be be
 void ChangeHash(struct stack* stk);         ///This function updates the hash value
 
+// TODO: This line of code defines a function with name HashOkData,
+//       which returns bool and accepts struct stack* stk which
+//       is a pointer to a struct called "stack" with name "stk".
+// 
+//       This function is used for Hash Ok Data
+// TODO: Rename, use verbs, please!
 bool HashOkData(struct stack* stk);         ///This function checks the expected and received hash
+// TODO: Rename, use verbs, please!
 bool HashOkStruct(struct stack* stk);       ///This function checks the expected and received hash
 
+
+// TODO: Include header which defines size_t :)
+// TODO: Rename, use verbs, please!
+// TODO: This is THE hash but you make it look like it is A hash.
 hash_t SumHash (void* object, size_t len);  ///This function considers the hash
 
+// TODO: Rename, use verbs, please!
 hash_t DataHash(struct stack* stk);         ///This function returns a new hash value
+// TODO: Rename, use verbs, please!
 hash_t StructHash(struct stack* stk);       ///This function returns a new hash value
 
 #endif // HASH_H_INCLUDED
--- Main.cpp
+++ Main.cpp
@@ -7,18 +7,17 @@
 
 int main(const int argc, const char* argv[])
     {
-    #ifdef LOG
-        OpenLog(argv[0]);
+    #ifdef LOG // TODO: Indentation?
+        OpenLog(argv[0]); // TODO: make OpenLog a macro that is defined or not defined depending on LOG?
     #endif
     struct stack stk = {};
     StackCtor(&stk, 20);
 
-    StackPush(&stk, 1);
-    StackPush(&stk, 2);
-    StackPush(&stk, 3);
-    StackPush(&stk, 4);
-    StackPush(&stk, 5);
-    StackPush(&stk, 6);
+    const int number_of_elements = 6;
+    for (int i = 1; i < number_of_elements; ++i)
+	{
+	StackPush(&stk, i);
+	}
 
     printf("\n hash in struct is %d\n", stk.struct_hash);
     hash_t k = SumHash(&stk, sizeof(struct stack));
