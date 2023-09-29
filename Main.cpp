8063df166024aeb03030ce9926fe441eb48ac7b9
nurikyet <142971916+nurikyet@users.noreply.github.com>
2023-09-30 00:19:34 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,15 @@
 #include "Stack.h"
 #include "logfile.h"
 
+
 int main(const int argc, const char* argv[])
     {
     #ifdef LOG
         OpenLog(argv[0]);
     #endif
     struct stack stk = {};
-    StackCtor(&stk, 2);
+    StackCtor(&stk, 20);
+
     StackPush(&stk, 1);
     StackPush(&stk, 2);
     StackPush(&stk, 3);
@@ -22,21 +24,6 @@ int main(const int argc, const char* argv[])
     hash_t k = SumHash(&stk, sizeof(struct stack));
     printf("SumHash is %u\n", k);
 
-    //stk.capacity = 5;
-    //stk.size = 6;
-    /*
-    StackPush(&stk, 1);
-    StackPush(&stk, 2);
-    StackPush(&stk, 3);
-    StackPush(&stk, 4);
-    StackPush(&stk, 5);
-    StackPush(&stk, 6);
     STACK_DUMP(&stk);
-
-    elem a = 1;
-    StackPop(&stk, &a);
-    StackPop(&stk, &a);
-    STACK_DUMP(&stk);
-    */
     StackDtor(&stk);
     }
