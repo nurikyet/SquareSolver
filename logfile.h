b40870e81cb49af1740192ab10c541e1ed00c40d
nurikyet <142971916+nurikyet@users.noreply.github.com>
2023-10-01 14:16:22 +0300

    Add files via upload

--- logfile.h
+++ logfile.h
@@ -1,11 +1,9 @@
-// TODO: Rename this file it's named differently from all other files!
-
 #ifndef LOGFILE_H_INCLUDED
 #define LOGFILE_H_INCLUDED
 
 #include <stdio.h>
 
-#include "Types.h" // TODO: remove this includes! They are not used in this header!
+#include "Types.h"
 #include "Stack.h"
 
 extern FILE* LOG_FILE;
